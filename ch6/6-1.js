a07e710df9a11138acf1f20d15b6d85a1b6c9fdf
Yangjisung <ygji6406@gmail.com>
2023-01-17 11:04:14 +0900

    6-1 함수 추출하기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,39 @@
+// 함수 추출하기
+// 1. 지역변수를 사용하는 곳에 가장 가깝게 위치시키는 것이 좋다.
+// 2. 산개된 코드를 하나씩 뭉쳐준다.
+// 3. 각 묶음 별로 함수로 추출한다.
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+function printBanner() {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+  // let result = 0;
+  // for (const o of invoice.orders) {
+  //   result += o.amount;
+  // }
+  // return result;
+}
 
-  // record due date
+function recordDueDate(invoice) {
   const today = new Date();
-  invoice.dueDate = new Date(
+  return (invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
-  );
+  ));
+}
 
-  //print details
+function printDetails(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
@@ -26,6 +41,7 @@ export function printOwing(invoice) {
 
 const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+  customer: "엘리",
 };
 printOwing(invoice);
+
