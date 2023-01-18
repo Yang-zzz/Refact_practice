5eb73e99763c370758132f96ab95423e3245d154
Yangjisung <ygji6406@gmail.com>
2023-01-18 11:32:45 +0900

    여러 함수를 변환 함수로 묶기

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,22 @@
+import _, { result } from 'lodash';
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+export function enrichReading(original) {
+  // return result = { ...original }; // object.assing : 얕은 복사만 됨. 
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.texableCharge = Math.max(
+    0,
+    result.baseCharge - texableCharge(result.year)
+  )
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate (reading.month, reading.year) * reading.quantity;
 }
 
 export function baseRate(month, year) {
