ab0bc56a7abec5d5f229ac4af2cb182daeb96a9d
Yangjisung <ygji6406@gmail.com>
2023-01-18 10:34:50 +0900

    변수 캡슐화하기_수정

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -23,5 +23,5 @@ let defaultOwner = new personalbar({ firstName: '마틴', lastName: '파울러' 
 export function getDefaultOwner() {
   return defaultOwner;
 }
-
-// 스프레드 기법은 얕은 복사로 살짝 위험!, 중첩객체가 있을 경우 문제가 될 수 있음.
+//방법1
+//방법2 리턴 값에 스프레드 기법을 사용해서, 원본객체의 변화를 주지 않고 다룰 수 있다. but, 스프레드 기법은 얕은 복사로 살짝 위험!, 중첩객체가 있을 경우 문제가 될 수 있음.
