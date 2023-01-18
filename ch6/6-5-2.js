68d4048961396fbff2fba701228d7eb8eafbb0b3
Yangjisung <ygji6406@gmail.com>
2023-01-18 10:08:22 +0900

    6-10_함수 선언 바꾸기

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -4,9 +4,11 @@ export default class Book {
     this.#reservations = [];
   }
 
-  addReservation(customer) {
+  addReservation(customer, isPriority = false) {
     this.#reservations.push(customer);
   }
+// 플래그 두번째 인자로 불리언 값을 받는 것은 좋지 않다.
+
 
   hasReservation(customer) {
     return this.#reservations.some(
--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,6 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+export function inNewEngland(state) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(state);
 }
+
+// 함수 내부에서 필요한 것 만 받아오고 , 외부의 의존성을 낮추는게 좋다.
+// 필요한것  == state
