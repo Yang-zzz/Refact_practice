9e793582085f6ea6222f0dc05f8f83bff76f5284
Yangjisung <ygji6406@gmail.com>
2023-01-19 11:15:57 +0900

    단계 쪼개기 챌린지

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,38 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
+  const basePrice = calculateBasePrice(product, quantity);
+  const discount = calculateDiscountedPrice(product, quantity);
+  const shippingCost = calculateShippinCost(
+    basePrice,
+    quantity,
+    shippingMethod
+  );
+  return basePrice - discount + shippingCost;
+
+  // 총 배송비 계산
+  //   const shippingCost = quantity * shippingPerCase;
+  //   const price = basePrice - discount + shippingCost;
+  //   return price;
+}
+
+function calculateBasePrice(product, quantity) {
+  return product.basePrice * quantity;
+}
+
+function calculateDiscountedPrice(product, quantity) {
+  return (
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
-    product.discountRate;
+    product.discountRate
+  );
+}
+
+function calculateShippinCost(basePrice, quantity, shippingMethod) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+
+  return quantity * shippingPerCase;
 }
 
 // 사용 예:
