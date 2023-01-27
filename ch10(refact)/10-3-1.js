8f79927e75cc806c462dea188f9faa53f8d0fc7e
Yangjisung <ygji6406@gmail.com>
2023-01-27 11:22:58 +0900

    중첩조건문을 보호구문으로 바꾸기

--- ch10(refact)/10-3-1.js
+++ ch10(refact)/10-3-1.js
@@ -1,22 +1,24 @@
 export function payAmount(employee) {
-  let result;
-  if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
+  if(employee !== numll) {
+    // 특정로직을 많이 처리하는 코드
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return
+  } // 특정조건이 아니라면 함수에서 빨리 나갈 수 있도록 작성하는 것이 중요
+
+  if (employee.isSeparated) {
+    return { amount: 0, reasonCode: "SEP" };
   }
-  return result;
+
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: "RET" };
+  } 
+    // lorem.ipsum(dolor.sitAmet);
+    // consectetur(adipiscing).elit();
+    // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    // ut.enim.ad(minim.veniam);
+    return someFinalComputation();
 }
 
 function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
+  return { amount: 999, reasonCode: "UNICORN" };
 }
--- ch10(refact)/10-3-2.js
+++ ch10(refact)/10-3-2.js
@@ -1,11 +1,16 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isEligeibleForAdjustCapital()) {
+    return 0;
   }
-  return result;
+  return (
+    (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+  );
+}
+
+function isEligeibleForAdjustCapital(instrument) {
+  return (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  );
 }
