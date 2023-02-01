64af52e103f6968b482d84043287f6e25dd0b2f2
Yangjisung <ygji6406@gmail.com>
2023-02-01 10:32:14 +0900

    질의함수와 변경함수 분리하기

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,35 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
+function TotalOutstanding() {
+  return result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
-  return result;
+}
+
+function sendBill() {
+  // SendBill
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+const miscreant = findMiscreant(people);
+setOffAlarms(alarm,miscreant);
+}
+
+function findMiscreant() {
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
       return 'Don';
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
       return 'John';
     }
   }
   return '';
 }
-
 function setOffAlarms(alarm, p) {
   alarm.setOff('Found Miscreant ' + p);
 }
+
+// 예상하지 못한 부수적인 역할이 있는 것 == 사이드 이펙트, 최대한 지양할 것
+// 다른 역할을 하지 않도록 하는 것이 중요
