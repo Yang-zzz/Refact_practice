9c9ffa98d9e133a509751506971c5f9e907869de
Yangjisung <ygji6406@gmail.com>
2023-02-01 10:56:00 +0900

    플래그 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -1,19 +1,22 @@
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function setWidth(value) {
+  this._width = value;
+}
+
+function setHeight(value) {
+  this._height = value;
 }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regulartBook(customer) {}
+  premiumtBook(customer) {}
+  #book(customer, isPremium) {}
 }
 
 // 예제 3
-function setSwitch(on);
+function SwitchOn();
+function SwitchOff();
+
+// 개별적 함수를 명령형으로 작성할 수 있도록 한다.
+// 함수를 만들 때 매개변수가 3개이상 넘어가지 않도록 한다.
